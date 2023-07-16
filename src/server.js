87cb859bc3d318699e067123c1c51a000a1ae6fb
thuha <thuharosy2003@gmail.com>
2023-07-16 17:45:57 +0700

    :sparkles: Login with facebook account

--- src/server.js
+++ src/server.js
@@ -6,7 +6,48 @@ import configViewEngine from "./config/viewEngine";
 import routes from "./routes/index";
 import configSession from "./config/session";
 import passport from "passport";
+import pem from "pem";
+import https from "https";
 
+pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+  if (err) {
+    throw err;
+  }
+  // Init app
+  const app = express();
+
+  // Connect to MongoDB
+  connectDB();
+
+  // Config session
+  configSession(app);
+
+  // Config view engine
+  configViewEngine(app);
+
+  // Enable post data for request
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // Enable flash messages
+  app.use(connectFlash());
+
+  // Config passport js
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Init routes
+  routes(app);
+
+  https
+    .createServer({ key: keys.clientKey, cert: keys.certificate }, app)
+    .listen(process.env.APP_PORT, process.env.APP_HOST, (req, res) => {
+      console.log(
+        `I'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`
+      );
+    });
+});
+
+/*
 // Init app
 const app = express();
 
@@ -32,11 +73,11 @@ app.use(passport.session());
 // Init routes
 routes(app);
 
-const hostName = "localhost";
-const port = process.env.APP_PORT || 8080;
 
-app.listen(8080, process.env.APP_HOST, (req, res) => {
+app.listen(process.env.APP_PORT, process.env.APP_HOST, (req, res) => {
   console.log(
     `I'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`
   );
 });
+
+*/
